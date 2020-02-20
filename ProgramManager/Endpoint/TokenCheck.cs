40e214c623b688685d0fbae83979fae71cc5c020
Xwilarg <xwilarg@yahoo.fr>
2020-02-20 23:49:11 +0900

    Add tokenCheck endpoint

---
+++ ProgramManager/Endpoint/TokenCheck.cs
@@ -0,0 +1,33 @@
+﻿using Nancy;
+
+namespace ProgramManager.Endpoint
+{
+    public class TokenCheck : NancyModule
+    {
+        public TokenCheck() : base("/tokenCheck")
+        {
+            Post("/", x =>
+            {
+                // Getting args
+                var args = Common.ParseArgs(Request.Body);
+
+                // Error handling
+                Common.Answer? error = Common.BasicCheck(true, args, "token");
+                if (error.HasValue)
+                    return (Response.AsJson(new Response.Information()
+                    {
+                        Message = error.Value.message
+                    }, error.Value.code));
+
+
+                if (!Program.P.ProgDb.IsTokenValid(args["token"])
+                    return (Response.AsJson(new Response.Information()
+                    {
+                        Message = "Invalid session token"
+                    }, HttpStatusCode.Unauthorized));
+
+                return (Response.AsJson(new Response.Empty()
+                { }, HttpStatusCode.NoContent));
+            });
+        }
+    }
